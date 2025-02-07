670b314615af6bf90ff223c2cfde45b931ad5e9c
briancosentino <cosentinobrian@gmail.com>
2025-02-07 14:45:46 +0100

    add second eercise

--- ex-js-variables/02-strings/script.js
+++ ex-js-variables/02-strings/script.js
@@ -1,11 +1,18 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+const name = 'Brian';
 // Stampa il valore di nome nella console.
+console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+const number = 1;
+const text = 'text'
+const bool = true;
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+console.log(number, text, bool);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +21,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + ' ' + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
